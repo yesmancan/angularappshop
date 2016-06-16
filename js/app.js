fef83564369b65755369743fa00d3c60398c6950
yesmancan <yesmancan_0611@hotmail.com>
2016-06-16 10:47:20 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+﻿//var app = angular.module("demo", ["ngRoute"]);
+
+//app.config(function ($routeProvider) {
+//    $routeProvider.
+//      when('/', {
+//          templateUrl: 'country-list.html',
+//          controller: 'CountryListCtrl'
+//      }).
+//      when('/country/:countryName', {
+//          templateUrl: 'country-detail.html',
+//          controller: 'CountryDetailCtrl'
+//      }).
+//      otherwise({
+//          redirectTo: '/'
+//      });
+//});
+
+//app.controller("CountryListCtrl", function ($scope, $http) {
+//    $http.get("country.json").success(function (res) {
+//        $scope.ulkeler = res.ulkeler;
+//    });
+//});
+
+//app.controller("CountryDetailCtrl", ["$scope", "$routeParams", function ($scope, $routeParams) {
+//    $scope.countryName = $routeParams.countryName
+//}]);
